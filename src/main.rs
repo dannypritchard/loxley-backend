a5506d50fe1857b17c2270c8f51601d0cc9ac81e
Danny Revenant <dannyrevenant@gmail.com>
2022-12-30 16:48:51 +0000

    Add Json and CORS

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,52 @@
 #[macro_use]
 extern crate rocket;
+use rocket::fairing::{Fairing, Info, Kind};
+use rocket::http::Header;
+use rocket::serde::{json::Json, Serialize};
+use rocket::{Request, Response};
+
+pub struct Cors;
+
+#[rocket::async_trait]
+impl Fairing for Cors {
+    fn info(&self) -> Info {
+        Info {
+            name: "Cross-Origin-Resource-Sharing Fairing",
+            kind: Kind::Response,
+        }
+    }
+
+    async fn on_response<'r>(&self, _request: &'r Request<'_>, response: &mut Response<'r>) {
+        response.set_header(Header::new("Access-Control-Allow-Origin", "*"));
+        response.set_header(Header::new(
+            "Access-Control-Allow-Methods",
+            "POST, PATCH, PUT, DELETE, HEAD, OPTIONS, GET",
+        ));
+        response.set_header(Header::new("Access-Control-Allow-Headers", "*"));
+        response.set_header(Header::new("Access-Control-Allow-Credentials", "true"));
+    }
+}
+
+#[derive(Serialize)]
+#[serde(crate = "rocket::serde")]
+struct HelloWorld {
+    hello: String,
+}
 
 #[get("/")]
-async fn index() -> &'static str {
-    "{\"hello\": \"world\"}"
+async fn index() -> Json<HelloWorld> {
+    Json(HelloWorld {
+        hello: "world i guess".to_owned(),
+    })
 }
 
 #[rocket::main]
 async fn main() -> Result<(), rocket::Error> {
-    let _rocket = rocket::build().mount("/", routes![index]).launch().await?;
+    let _rocket = rocket::build()
+        .attach(Cors)
+        .mount("/", routes![index])
+        .launch()
+        .await?;
 
     Ok(())
 }
